082e6465022100759135a705711e39a464d6b49f
Karina Mio <me@karinamio.com>
2016-09-10 11:50:18 -0400

    hooked up clarifai api

--- server/venv/server.py
+++ server/venv/server.py
@@ -1,10 +1,35 @@
+import pprint
+
 from flask import Flask
+from clarifai.client import ClarifaiApi
 app = Flask(__name__)
+
 
 # index
 @app.route("/")
 def index():
-    return "o hai."
+	clarifai_api = ClarifaiApi() # assumes environment variables are set.
+	result = clarifai_api.tag_images(open('green_can2.jpg', 'rb'))
+
+	# pp = pprint.PrettyPrinter(indent = 4)
+	# pp.pprint(result)
+
+	clarifai_results = {}
+	tag_results = result[u'results'][0][u'result'][u'tag'][u'classes']
+	probability_results = result[u'results'][0][u'result'][u'tag'][u'probs']
+	for index in enumerate(tag_results):
+		probability = round(probability_results[index[0]] * 100)
+		tag = str(tag_results[index[0]])
+		
+		clarifai_results[probability] = tag
+
+	results = []
+	for probability in (sorted(clarifai_results, reverse = True)):
+		results.append((clarifai_results[probability], probability))
+	
+	print results
+
+	return "o hai."
 
 # endpoint for receiving inbound messages
 @app.route("/messages")
