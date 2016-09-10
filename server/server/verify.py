abce9986a8b2144b8b7e4eabc94c883c3007ecd4
Karina Mio <me@karinamio.com>
2016-09-10 15:56:11 -0400

    hooked up to clarifai

--- server/server/verify.py
+++ server/server/verify.py
@@ -1,21 +1,19 @@
-from server import app
 import json
 import os
+
+from server import app
 from flask import request
+from clarifai.client import ClarifaiApi
 
 ALLOWED_EXTENSIONS = set(['jpg', 'png'])
+
 
 def allowed_file(filename):
     return '.' in filename and \
             filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/verify', methods=['POST'])
-def response():
-    print request.files['image'].filename
-    print request.form['name']
-    # see all the keys
-    #print request.__dict__
 
+def validate_request(request, filename):
     error_reason = ''
 
     if 'image' not in request.files:
@@ -23,9 +21,7 @@ def response():
         error_message = {'error': error_reason}
         return json.dumps(error_message), 400
 
-    file = request.files['image']
-
-    if not file or not allowed_file(file.filename):
+    if not file or not allowed_file(filename):
         error_reason = 'not allowed filename'
 
     if 'name' not in request.form:
@@ -39,6 +35,36 @@ def response():
         error_message = {'error': error_reason}
         return json.dumps(error_message), 400
 
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-    return json.dumps({ 'associations': [], 'result': True }), 200
+
+def verify_with_clarify(filename):
+    clarifai_api = ClarifaiApi()  # assumes environment variables are set.
+    result = clarifai_api.tag_images(open('uploads/' + filename, 'rb'))
+
+    api_results = {}
+    tag_results = result[u'results'][0][u'result'][u'tag'][u'classes']
+    probability_results = result[u'results'][0][u'result'][u'tag'][u'probs']
+    for index in enumerate(probability_results):
+        probability = round(probability_results[index[0]] * 100)
+        tag = str(tag_results[index[0]])
+
+        api_results[tag] = probability
+
+    # results = {}
+    # for probability in (sorted(clarifai_results, reverse = True)):
+    #     results[clarifai_results[probability]] = probability
+
+    if request.form['name'] in api_results:
+        return True
+    else:
+        return False
+
+@app.route('/verify', methods=['POST'])
+def response():
+    filename = request.files['image'].filename
+
+    validate_request(request, filename)
+    verification = verify_with_clarify(filename)
+
+    return json.dumps({ 'associations': [], 'result': verification }), 200
